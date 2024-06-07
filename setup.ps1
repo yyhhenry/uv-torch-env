207ea320cf4526089af758ed99a96cdde17b6bf6
yyhhenry <yyhhenry@foxmail.com>
2024-06-07 11:25:59 +0800

    添加Force选项

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,8 @@
-if (-not (Test-Path .venv)) {
+param (
+    [switch]$Force
+)
+
+if (-not (Test-Path .venv) -or $Force) {
     uv venv -p 3.10
     uv pip install torch==2.1.2+cu121 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
     uv pip install -r requirements.txt
