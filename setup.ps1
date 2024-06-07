deb1ee0f3bfef6fa1253e4e6c131060211f9fe9b
yyhhenry <yyhhenry@foxmail.com>
2024-06-07 11:28:34 +0800

    Update setup scripts and add Force option

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,14 @@ param (
     [switch]$Force
 )
 
-if (-not (Test-Path .venv) -or $Force) {
+$needInstall = $Force
+
+if (-not (Test-Path .venv)) {
     uv venv -p 3.10
+    $needInstall = $true
+}
+
+if ($needInstall) {
     uv pip install torch==2.1.2+cu121 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
     uv pip install -r requirements.txt
 }
