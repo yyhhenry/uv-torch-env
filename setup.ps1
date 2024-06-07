dc2e1510d20f03f641587d381068f6a74c0cff99
yyhhenry <yyhhenry@foxmail.com>
2024-06-07 11:53:04 +0800

    更改为uv.toml形式

--- setup.ps1
+++ setup.ps1
@@ -1,17 +1,7 @@
-param (
-    [switch]$Force
-)
-
-$needInstall = $Force
-
 if (-not (Test-Path .venv)) {
     uv venv -p 3.10
-    $needInstall = $true
 }
 
-if ($needInstall) {
-    uv pip install torch==2.1.2+cu121 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-    uv pip install -r requirements.txt
-}
+uv pip install -r requirements.txt
 
 .venv\Scripts\activate
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 if [ ! -d ".venv" ]; then
     uv venv -p 3.10
-    uv pip install torch==2.1.2+cu121 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-    uv pip install -r requirements.txt
 fi
 
+uv pip install -r requirements.txt
 source .venv/bin/activate
