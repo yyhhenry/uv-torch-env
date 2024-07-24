00f40c134a0b14a1d79e82e3a8e305c1f093227a
yyhhenry <yyhhenry@foxmail.com>
2024-07-24 19:02:58 +0800

    分离torch部分

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,10 @@ if (-not (Test-Path .venv)) {
     uv venv -p 3.10
 }
 
+if (Test-Path requirements-torch.txt) {
+    uv pip install -r requirements-torch.txt --extra-index-url https://download.pytorch.org/whl/cu121
+}
+
 uv pip install -r requirements.txt
 
 .venv\Scripts\activate
