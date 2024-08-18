bc28c852c5f54aafe06e9bf80f3003d6a9a61e8e
yyhhenry <yyhhenry@foxmail.com>
2024-08-18 19:49:39 +0800

    Update env_test.py

--- env_test.py
+++ env_test.py
@@ -1,47 +1,37 @@
 def test_python():
     import sys
 
-    print("python", sys.version)
+    print(f"{sys.version=}")
 
 
 def test_numpy():
-    import numpy as np
+    import numpy
 
-    print("numpy", np.__version__)
-
-    x = np.array([1, 2, 3])
-    y = np.array([1, 4, 9])
-    assert (x * x == y).all()
-    print("numpy works")
+    print(f"{numpy.__version__=}")
 
 
 def test_torch():
     import torch
 
-    print("torch", torch.__version__)
-
-    x = torch.Tensor([1, 2, 3])
-    y = torch.Tensor([1, 4, 9])
-    assert bool((x * x == y).all().item())
-    print("torch works")
+    print(f"{torch.__version__=}")
 
 
 def test_torchaudio():
     import torchaudio
 
-    print("torchaudio", torchaudio.__version__)
+    print(f"{torchaudio.__version__=}")
 
 
 def test_torchvision():
     import torchvision
 
-    print("torchvision", torchvision.__version__)
+    print(f"{torchvision.__version__=}")
 
 
 def test_cuda():
     import torch
 
-    print("cuda", torch.cuda.is_available())
+    print(f"{torch.cuda.is_available()=}")
 
     x = torch.Tensor([1, 2, 3]).cuda()
     y = torch.Tensor([1, 4, 9]).cuda()
