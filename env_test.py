a7932923f2f6dd0054793f3e4fa4cc7577f68119
yyhhenry <yyhhenry@foxmail.com>
2024-08-18 19:46:47 +0800

    Update env_test.py

--- env_test.py
+++ env_test.py
@@ -1,24 +1,58 @@
-import sys
+def test_python():
+    import sys
 
-import numpy as np
-import torch
-import torchaudio
-import torchvision
-from torch import Tensor
+    print("python", sys.version)
 
-print("python", sys.version)
 
-print("numpy", np.__version__)
+def test_numpy():
+    import numpy as np
 
-print("torch", torch.__version__)
+    print("numpy", np.__version__)
 
-print("torchaudio", torchaudio.__version__)
+    x = np.array([1, 2, 3])
+    y = np.array([1, 4, 9])
+    assert (x * x == y).all()
+    print("numpy works")
 
-print("torchvision", torchvision.__version__)
 
-print("cuda", torch.cuda.is_available())
+def test_torch():
+    import torch
 
-x = Tensor([1, 2, 3]).cuda()
-y = Tensor([1, 4, 9]).cuda()
-assert bool((x * x == y).all().item())
-print("torch.cuda works")
+    print("torch", torch.__version__)
+
+    x = torch.Tensor([1, 2, 3])
+    y = torch.Tensor([1, 4, 9])
+    assert bool((x * x == y).all().item())
+    print("torch works")
+
+
+def test_torchaudio():
+    import torchaudio
+
+    print("torchaudio", torchaudio.__version__)
+
+
+def test_torchvision():
+    import torchvision
+
+    print("torchvision", torchvision.__version__)
+
+
+def test_cuda():
+    import torch
+
+    print("cuda", torch.cuda.is_available())
+
+    x = torch.Tensor([1, 2, 3]).cuda()
+    y = torch.Tensor([1, 4, 9]).cuda()
+    assert bool((x * x == y).all().item())
+    print("torch.cuda works")
+
+
+if __name__ == "__main__":
+    test_python()
+    test_numpy()
+    test_torch()
+    test_torchaudio()
+    test_torchvision()
+    test_cuda()
